256a5328a39611956f9f04bec28500d07251bb7a
Shaji <2406756@cognizant.com>
2025-05-16 13:17:35 +0530

    for, while loop

---
+++ Codes/iteration(forloop)-1.cs
@@ -0,0 +1,9 @@
+using System;
+class iterations{
+    static void Main(){
+        Console.WriteLine("---- For loop ----");
+        for(int i=4 ; i<40 ; i++){
+            Console.WriteLine("Number " +i);
+        }
+    }
+}
---
+++ Codes/iteration(whileloop)-1.cs
@@ -0,0 +1,11 @@
+using System;
+class iteration{
+    static void Main(){
+        Console.WriteLine("---- While Loop ----");
+        int i = 4;
+        while(i <= 40){
+            Console.WriteLine("Number -> " +i);
+            i++;
+        }
+    }
+}
